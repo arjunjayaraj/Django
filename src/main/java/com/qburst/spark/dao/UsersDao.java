ac908ff59da3fc85b9636590f383f937ae7e0e0a
Arjun k <k.arjunjayaraj@gmail.com>
2015-06-19 17:22:24 +0530

    completed Crudrepository and cache of files

--- src/main/java/com/qburst/spark/dao/UsersDao.java
+++ src/main/java/com/qburst/spark/dao/UsersDao.java
@@ -1,7 +1,9 @@
 package com.qburst.spark.dao;
 import com.qburst.spark.model.*;
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 public interface UsersDao extends CrudRepository<User, String>{
 
 }
--- src/main/java/com/qburst/spark/service/UserServiceImpl.java
+++ src/main/java/com/qburst/spark/service/UserServiceImpl.java
@@ -1,41 +1,58 @@
 package com.qburst.spark.service;
 
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.qburst.spark.dao.UserDao;
+import com.qburst.spark.dao.UsersDao;
 import com.qburst.spark.model.User;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	private UserDao userDao;
+
 	@Override
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
-	
-	@Override
-@Transactional
-	public void addUser(User user) {
-		User entity = this.userDao.find(User.class, user.getUsername());
-		if (entity == null) {
-		this.userDao.create(user);
-		}
+
+	@Autowired
+	private UsersDao usersDao;
+
+	public void setUsersDao(UsersDao usersDao) {
+		this.usersDao = usersDao;
 	}
+
+	 private static final Logger LOGGER =
+	 LoggerFactory.getLogger(UserServiceImpl.class);
+
+	@Override
+	@Transactional
+	public void addUser(User user) {
+		LOGGER.info("Creating a new user entry by using information: {}", user);
+		if (!this.usersDao.exists(user.getUsername())) {
+			this.usersDao.save(user);
+			User entity2 = this.usersDao.findOne(user.getUsername());
+			System.out.println("Username" + entity2.getUsername());
+			System.out.println("Password" + entity2.getPassword());
+
+		}
+
+	}
+
 	@Override
 	@Transactional
 	public void updateUser(User user) {
 		User oldInfo = this.userDao.find(User.class, user.getUsername());
-		if(oldInfo!=null){
+		if (oldInfo != null) {
 			oldInfo.setPassword(user.getPassword());
 			this.userDao.update(oldInfo);
-		}
-		else{
+		} else {
 			System.out.println("No such entity found in update");
 		}
 	}
